a407e5617884cbb436756ebd0f94b203ebf552cc
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-09-18 20:31:25 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -29,16 +29,19 @@ class TempVars:
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 # Command to set caption
 @StreamBot.on_message(filters.group & filters.command('set_caption'))
 async def add_caption(c: Client, m: Message):
     if len(m.command) == 1:
         buttons = [[InlineKeyboardButton('⇇ ᴄʟᴏsᴇ ⇉', callback_data='close')]]
-        return await m.reply_text(
+        await m.reply_text(
             "**ʜᴇʏ 👋\n\n<u>ɢɪᴠᴇ ᴛʜᴇ ᴄᴀᴩᴛɪᴏɴ</u>\n\nᴇxᴀᴍᴩʟᴇ:- `/set_caption <b>{file_name}\n\nSize : {file_size}\n\n➠ Fast Download Link :\n{download_link}\n\n➠ Watch Link : {watch_link}\n\n☠️ Powered By : @OMGxLinks</b>`**",
             reply_markup=InlineKeyboardMarkup(buttons)
         )
+        return
+
     caption = m.text.split(" ", 1)[1]
     await db.set_caption(m.from_user.id, caption=caption)
     buttons = [[InlineKeyboardButton('⇇ ᴄʟᴏsᴇ ⇉', callback_data='close')]]
@@ -52,7 +55,9 @@ async def add_caption(c: Client, m: Message):
 async def delete_caption(c: Client, m: Message):
     caption = await db.get_caption(m.from_user.id)
     if not caption:
-        return await m.reply_text("__**😔 You don't have any caption**__")
+        await m.reply_text("__**😔 You don't have any caption**__")
+        return
+
     await db.set_caption(m.from_user.id, caption=None)
     buttons = [[InlineKeyboardButton('⇇ ᴄʟᴏsᴇ ⇉', callback_data='close')]]
     g = await m.reply_text(f"<b>ʜᴇʏ {m.from_user.mention}\n\n✅ Caption successfully deleted</b>", reply_markup=InlineKeyboardMarkup(buttons))
@@ -70,10 +75,11 @@ async def see_caption(c: Client, m: Message):
         await m.reply_text("__**😔 You don't have any caption**__")
 
 # Handle media messages
-@StreamBot.on_message((filters.group) & (filters.document | filters.video | filters.audio | filters.photo), group=4)
+@StreamBot.on_message(filters.group & (filters.document | filters.video | filters.audio | filters.photo), group=4)
 async def private_receive_handler(c: Client, m: Message):
     if str(m.chat.id).startswith("-100") and m.chat.id not in Var.GROUP_ID:
         return
+
     elif m.chat.id not in Var.GROUP_ID:
         if not await db.is_user_exist(m.from_user.id):
             await db.add_user(m.from_user.id)
@@ -84,46 +90,60 @@ async def private_receive_handler(c: Client, m: Message):
             return
 
     media = m.document or m.video or m.audio
-    file_name = f"{m.caption}" if m.caption else ""
+    file_name = m.caption or ""
     file_name = file_name.replace(".mkv", "").replace("HEVC", "#HEVC").replace("Sample video.", "#SampleVideo").replace(".", " ")
 
     try:
         user = await db.get_user(m.from_user.id)
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        
-        hs_stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+
+        # Generate stream and download links
+        hs_stream_link = f"{Var.URL}watch/{log_msg.id}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         stream_link = await short_link(hs_stream_link, user)
 
-        hs_online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        hs_online_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = await short_link(hs_online_link, user)
 
         await log_msg.reply_text(
-            text=f"**Requested by :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID :** `{m.from_user.id}`\n**Stream link :** {stream_link}",
-            disable_web_page_preview=True,
-            quote=True
+            f"**Requested by :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID :** `{m.from_user.id}`\n**Stream link :** {stream_link}",
+            disable_web_page_preview=True, quote=True
         )
-        
-        c_caption = await db.get_caption(m.from_user.id)
-        if c_caption:
-            caption = c_caption.format(file_name=file_name, file_size=humanbytes(get_media_file_size(m)), download_link=online_link, watch_link=stream_link)
-        else:
-            caption = None
 
+        # Caption formatting
+        c_caption = await db.get_caption(m.from_user.id)
+        caption = None
+        if c_caption:
+            caption = c_caption.format(
+                file_name=file_name,
+                file_size=humanbytes(get_media_file_size(m)),
+                download_link=online_link,
+                watch_link=stream_link
+            )
+
+        # Send cached media with the caption
         await c.send_cached_media(
-            caption=caption,
             chat_id=m.chat.id,
-            file_id=media.file_id
+            file_id=media.file_id,
+            caption=caption
         )
-        
+
         buttons = [[InlineKeyboardButton('⇇ ᴄʟᴏsᴇ ⇉', callback_data='close')]]
         await m.reply(
             "<b>❗⚠️ Important Notice! This file will be deleted in 10 minutes due to overuse! Please forward/save it!</b>",
             reply_markup=InlineKeyboardMarkup(buttons)
         )
+
     except FloodWait as e:
-        logging.error(f"FloodWait encountered: {str(e.x)}s")
+        logger.error(f"FloodWait encountered: {e.x} seconds.")
         await asyncio.sleep(e.x)
-        await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Got FloodWait of {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**User ID :** `{str(m.from_user.id)}`", disable_web_page_preview=True)
+        await c.send_message(
+            chat_id=Var.BIN_CHANNEL,
+            text=f"Got FloodWait of {e.x} seconds from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**User ID :** `{m.from_user.id}`",
+            disable_web_page_preview=True
+        )
+    except Exception as e:
+        logger.error(f"Error processing media message: {str(e)}")
+        await m.reply("❌ Failed to process your request due to an internal error.")
 
 # Shorten link with optional user details
 async def short_link(link, user=None):
@@ -133,16 +153,16 @@ async def short_link(link, user=None):
     api_key = user.get("shortner_api")
     base_site = user.get("shortner_url")
 
-    if bool(api_key and base_site) and Var.USERS_CAN_USE:
+    if api_key and base_site and Var.USERS_CAN_USE:
         shortzy = Shortzy(api_key, base_site)
         try:
             link = await shortzy.convert(link)
         except aiohttp.ClientResponseError as e:
-            logging.error(f"ClientResponseError: {e.status}, message='{e.message}', url={e.request_info.url}")
-            return link  # Return the original link if conversion fails
+            logger.error(f"ClientResponseError: {e.status}, message='{e.message}', url={e.request_info.url}")
         except Exception as e:
-            logging.error(f"Unexpected error: {e}")
-            return link  # Return the original link if any other error occurs
+            logger.error(f"Unexpected error: {e}")
+        finally:
+            return link
 
     return link
 
@@ -159,4 +179,3 @@ async def close_button(c: Client, cb: CallbackQuery):
 # Run the bot
 if __name__ == "__main__":
     StreamBot.run()
-            
