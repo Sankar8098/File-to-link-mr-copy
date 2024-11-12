45208c941d93f329a8da322189f390c9a707e126
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-11-12 05:48:21 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -1,11 +1,6 @@
-# (c) adarsh-goel
-import os
-import sys
-import glob
+# Adarsh/__main__.py
 import asyncio
 import logging
-import importlib
-from pathlib import Path
 from pyrogram import idle
 from .bot import StreamBot
 from .vars import Var
@@ -13,6 +8,10 @@ from aiohttp import web
 from .server import web_server
 from .utils.keepalive import ping_server
 from Adarsh.bot.clients import initialize_clients
+import importlib
+import sys
+import glob
+from pathlib import Path
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,68 +19,58 @@ logging.basicConfig(
 )
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
+# Path to plugins
 ppath = "Adarsh/bot/plugins/*.py"
 files = glob.glob(ppath)
-StreamBot.start()
+
+# Initialize event loop
 loop = asyncio.get_event_loop()
 
-
 async def start_services():
-    print('\n')
-    print('------------------- Initalizing Telegram Bot -------------------')
+    print('------------------- Initializing Telegram Bot -------------------')
+    await StreamBot.start()
     bot_info = await StreamBot.get_me()
     StreamBot.username = bot_info.username
-    print("------------------------------ DONE ------------------------------")
-    print()
-    print(
-        "---------------------- Initializing Clients ----------------------"
-    )
+    print(f"Bot Username: {StreamBot.username}")
+
+    print("\n---------------------- Initializing Clients ----------------------")
     await initialize_clients()
-    print("------------------------------ DONE ------------------------------")
-    print('\n')
-    print('--------------------------- Importing ---------------------------')
+    
+    print('\n--------------------------- Importing Plugins ---------------------------')
     for name in files:
         with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem.replace(".py", "")
+            plugin_name = Path(a.name).stem
             plugins_dir = Path(f"Adarsh/bot/plugins/{plugin_name}.py")
-            import_path = ".plugins.{}".format(plugin_name)
+            import_path = f".plugins.{plugin_name}"
             spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
-            sys.modules["Adarsh.bot.plugins." + plugin_name] = load
-            print("Imported => " + plugin_name)
+            sys.modules[f"Adarsh.bot.plugins.{plugin_name}"] = load
+            print(f"Imported => {plugin_name}")
+    
     if Var.ON_HEROKU:
-        print("------------------ Starting Keep Alive Service ------------------")
-        print()
+        print("\n------------------ Starting Keep Alive Service ------------------")
         asyncio.create_task(ping_server())
-    print('-------------------- Initalizing Web Server -------------------------')
+
+    print('-------------------- Initializing Web Server -------------------------')
     app = web.AppRunner(await web_server())
     await app.setup()
-    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADRESS
+    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
     await web.TCPSite(app, bind_address, Var.PORT).start()
-    print('----------------------------- DONE ---------------------------------------------------------------------')
-    print('\n')
-    print('---------------------------------------------------------------------------------------------------------')
-    print('---------------------------------------------------------------------------------------------------------')
-    print(' follow me for more such exciting bots! https://github.com/ITZ-JEOL')
-    print('---------------------------------------------------------------------------------------------------------')
-    print('\n')
-    print('----------------------- Service Started -----------------------------------------------------------------')
-    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
-    print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
-    print('                        Owner =>> {}'.format((Var.OWNER_USERNAME)))
+    
+    print('\n----------------------- Service Started ---------------------------')
+    print(f"Bot =>> {bot_info.first_name}")
+    print(f"Server IP =>> {bind_address}:{Var.PORT}")
+    print(f"Owner =>> {Var.OWNER_USERNAME}")
     if Var.ON_HEROKU:
-        print('                        app runnng on =>> {}'.format(Var.FQDN))
-    print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/Itz-JEOL/File-To-Link  also follow me for new bots')
-    print('---------------------------------------------------------------------------------------------------------')
+        print(f"App running on =>> {Var.FQDN}")
+    print('--------------------------------------------------------------')
     await idle()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         loop.run_until_complete(start_services())
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
+        
